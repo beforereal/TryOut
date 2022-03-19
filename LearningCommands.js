91737225252670bc61f0550931fa5f6956e39349
Furkan Canga <furkancanga@gmail.com>
2022-03-19 12:16:58 +0300

    Learning is a progress

--- LearningCommands.js
+++ LearningCommands.js
@@ -1,12 +1,11 @@
-
+  /*
   // learning how to comment on my code //
   console.log('my age is')
   console.log(25);
   console.log('I am trying to learn coding to be the best coder in the universe')
   console.log(4);
-  /* another style of comment
-  with many lines 
-  */
+   
+  
   
   console.log("This is a string");
   console.log(7); //This is a number
@@ -18,5 +17,49 @@
   console.log(22-14); //prints 8
   console.log(12*12);  //prints 144
 
+  */
+    
+  var a;
+  var b=7;
+
+  a=4;
+
+  a=b;
+
+  console.log(a);
 
 
+  a-= 8;
+  var c;
+  c = 21
+  c /= c;
+
+  c += 24
+
+  c *= 4
+
+  console.log(c);
+
+  var firstname="Furkan";
+
+  var lastname="Canga"
+
+  /* I didnt understnad this part
+
+  var UsingQuoteMark = "This is a /" example of how to use /" quoting mark";
+
+  console.log(UsingQuoteMark);
+
+*/
+
+console.log('Teaching the world how to code'.length);
+
+//how to random a number
+
+console.log(Math.floor(Math.random()*100));
+
+// Math library codes
+
+console.log(Math.floor(Math.random()*100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2022));
